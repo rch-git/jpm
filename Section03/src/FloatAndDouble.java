8166de9c94006e9426a3a700b45826938a784e24
Ravi <ravi@powercosts.com>
2019-10-24 12:59:33 -0500

    eclipse conversion

---
+++ Section03/src/FloatAndDouble.java
@@ -0,0 +1,30 @@
+
+public class FloatAndDouble
+{
+
+	public static void main(String[] args)
+	{
+		float myMinFloatValue = Float.MIN_VALUE;
+		float myMaxFloatValue = Float.MAX_VALUE;
+		System.out.println("Float minimum: " + myMinFloatValue);
+		System.out.println("Float maximum: " + myMaxFloatValue);
+		
+		double myMinDoubleValue = Double.MIN_VALUE;
+		double myMaxDoubleValue = Double.MAX_VALUE;
+		System.out.println("Double minimum: " + myMinDoubleValue);
+		System.out.println("Double maximum: " + myMaxDoubleValue);
+		
+		int myIntValue = 5 / 2;
+		float myFloatValue = 5.25f / 2f;
+		double myDoubleValue = 5.25d / 2d; //not necessary to specify d when using double
+		System.out.println("myIntValue: " + myIntValue);
+		System.out.println("myFloatValue: " + myFloatValue);
+		System.out.println("myDoubleValue: " + myDoubleValue);
+		
+		double pounds = 5;
+		double kilograms = pounds * 0.45359237;
+		System.out.println("kilograms: " + kilograms);
+
+	}
+
+}
---
+++ Section03/src/Operators.java
@@ -0,0 +1,27 @@
+
+public class Operators
+{
+
+	public static void main(String[] args)
+	{
+		int result = 1 + 2;
+		System.out.println(result);
+		
+		result /= 3;
+		System.out.println(result);
+		
+		boolean isAlien = false;
+		
+		if (isAlien == false)
+		{
+			System.out.println("Not an alien.");
+		}
+		
+		int topScore = 100;
+		if (topScore > 100)
+		{
+			System.out.println("High score.");
+		}
+	}
+
+}
