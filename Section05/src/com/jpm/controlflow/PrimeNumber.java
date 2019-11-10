4c651c9feddeda46433d5dd183da9e49a28ff1af
Ravi <ravi@powercosts.com>
2019-11-10 13:15:11 -0600

    section 5

---
+++ Section05/src/com/jpm/controlflow/PrimeNumber.java
@@ -0,0 +1,42 @@
+package com.jpm.controlflow;
+
+public class PrimeNumber
+{
+
+	public static void main(String[] args)
+	{
+		 boolean isPrime = isPrime(6);
+		 System.out.println(isPrime);
+		 
+		 for (int i = 0; i < 50; i++)
+		 {
+			 if (isPrime(i))
+			 {
+				 System.out.println("Number = " + i);
+			 }
+			 
+		 }
+
+	}
+	
+	public static boolean isPrime(int number)
+	{	
+		if (number == 1 || number == 0)
+		{
+			return false;
+		}
+		else
+		{
+			for (int i = 2; i <= number/2; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}	
+			}
+			
+			return true;
+		}
+	}
+
+}
