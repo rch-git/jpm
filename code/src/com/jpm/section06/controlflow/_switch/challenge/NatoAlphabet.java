9ecd804b347f0e921eb1e6e090622d308813bcc3
RC <rch@fastmail.com>
2024-01-10 04:36:49 -0600

    local

---
+++ code/src/com/jpm/section06/controlflow/_switch/challenge/NatoAlphabet.java
@@ -0,0 +1,30 @@
+package com.jpm.section06.controlflow._switch.challenge;
+
+public class NatoAlphabet
+{
+	public static void main(String[] args)
+	{
+		char c = 'A';
+		System.out.println("NATO Alphabet: " + natoAlphabet(c));
+	}
+	
+	public static String natoAlphabet (char c)
+	{
+		switch(c)
+		{
+			case 'A':
+				return "Able";
+			case 'B':
+				return "Baker";
+			case 'C':
+				return "Charlie";
+			case 'D':
+				return "Dog";
+			case 'E':
+				return "Easy";
+			default:
+				return "BAD";
+			
+		}
+	}
+}
